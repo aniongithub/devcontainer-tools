336b69a97aaa5f22f0cc23696bb270229bd08c73
Ani Balasubramaniam <ani@anionline.me>
2019-10-14 07:33:55 +0000

    Add working development devcontainer desc! Removed in-container installation of devcontainer for now, issues with acls. Added launch.json with predefined args for quick development.

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,3 @@ ENV VERSION=${VERSION}
 
 RUN apt-get update && \
     apt-get install -y git nano wget
-
-WORKDIR /usr/local/src/devcontainer/
-COPY . /usr/local/src/devcontainer/
-RUN dotnet pack && \
-    dotnet tool install --global --add-source devcontainer/nupkg devcontainer
-
-ENV PATH="$PATH:/root/.dotnet/tools"
---
+++ devcontainer/DevcontainerDesc.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace devcontainer
+{
+    public sealed class DevcontainerDesc
+    {
+        public string name { get; set; }
+        public string workspaceFolder { get; set; }
+        public string dockerComposeFile { get; set; }
+        public string service { get; set; }
+        public string shutdownAction { get; set; }
+    }
+}
